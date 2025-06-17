e70c44ec49a46248af443a2f7ebe988d554296f3
Matheus <matheusousaa@gmail.com>
2025-06-16 21:00:02 -0300

    updated table name in the migration that add new field to orders table

--- database/migrations/2025_02_28_205700_add_status_id_to_orders_table.php
+++ database/migrations/2025_02_28_205700_add_status_id_to_orders_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         
         Schema::table('orders', function (Blueprint $table) {
-            $table->foreignId('status_id')->constrained('order_status');
+            $table->foreignId('status_id')->constrained('order_statuses');
         });
     }
 
