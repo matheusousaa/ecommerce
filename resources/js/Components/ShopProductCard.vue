4b8e43bb2fde752fc6cece16ef863140120e3112
Matheus <matheusousaa@gmail.com>
2025-10-02 22:50:17 -0300

    changed shoppingCartIcon nomenclature

--- resources/js/Components/ShopProductCard.vue
+++ resources/js/Components/ShopProductCard.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue';
 import { useForm } from '@inertiajs/vue3';
-import { ShoppingCart } from 'lucide-vue-next';
+import { ShoppingCartIcon } from 'lucide-vue-next';
 
 const { product } = defineProps({
     product: Object
@@ -36,7 +36,7 @@ const formatPrice = (price) => {
                   :disabled="product.quantity <= 0"
                   class="px-4 py-2 bg-primary text-white text-sm font-medium rounded-lg hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed transition-colors"
                 >
-                    <ShoppingCart class="h-4 w-4 inline mr-1" />
+                    <ShoppingCartIcon class="h-4 w-4 inline mr-1" />
                     Add to Cart
                 </button>
             </div>
